514763709f7c86e4a26916778f3023bfb5abc897
Jacob <jakeboone@aol.com>
2016-01-09 10:12:37 -0500

    some changes to contact email and contact link

---
+++ models/submission.js
@@ -0,0 +1,17 @@
+// var mongoose =require('mongoose');
+
+
+// var Newcontact = mongoose.model('Newcontact',{
+// 	firstName: String,
+// 	lastName: String,
+// 	email: String,
+// 	phone:String,
+// 	reason:String,
+// 	message:String
+
+// })
+
+
+
+
+// module.exports = Newcontact
