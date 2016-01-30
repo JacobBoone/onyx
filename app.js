06594b2b27975d0a8282b9243517a11a65c594b6
Jacob <jakeboone@aol.com>
2016-01-30 08:15:11 -0500

    anchortags to jump to page sections

--- app.js
+++ app.js
@@ -1,37 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
-// var nodemailer = require('nodemailer');
-
-// var smtpTransport = nodemailer.createTransport("SMTP",{
-//    service: "Gmail",  // sets automatically host, port and connection security settings
-//    auth: {
-//        user: "example@example.com",
-//        pass: "example"
-//    }
-// });
-
-
-// smtpTransport.sendMail({  //email options
-//    from: "Sender Name <email@gmail.com>", // sender address.  Must be the same as authenticated user if using GMail. ME- so can i make this the user who submitted the form?
-//    to: "Receiver Name <receiver@email.com>", // receiver
-//    subject: "Emailing with nodemailer", // subject
-//    text: "Email Example with nodemailer" // body
-// }, function(error, response){  //callback
-//    if(error){
-//        console.log(error);
-//    }else{
-//        console.log("Message sent: " + response.message);
-//    }
-   
-//    smtpTransport.close(); // shut down the connection pool, no more messages.  Comment this line out to continue sending emails.
-// });
-
-
+var nodemailer = require('nodemailer');
 
 
 
 var indexController = require('./controllers/index.js');
+var contactController = require('./controllers/contact.js')
 
 var app = express();
 app.set('view engine', 'jade');
@@ -39,6 +13,8 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
+
+//pages
 app.get('/', indexController.index);
 app.get('/contact', indexController.contact);
 app.get('/contactemail', indexController.contactemail);
@@ -46,6 +22,9 @@ app.get('/birth', indexController.birth);
 app.get('/psychk', indexController.psychk);
 app.get('/essentialoils', indexController.essentialoils);
 app.get('/aboutme', indexController.aboutme);
+
+
+//contact form
 
 
 
