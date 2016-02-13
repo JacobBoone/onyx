5e89220ce36f375861c19c1364466b04bfdeaa2d
Jacob <jakeboone@aol.com>
2016-02-13 08:37:06 -0500

    facebook page icon link and footer cpright

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,24 +1,3 @@
-
-
-var windowWidth = $(window).width();
-var content = $('content')
-
-
-$(changeSize)function(){
-	if (windowWidth < 517).addClass('small-content').removeClass('large-content')
-		else .removeClass('small-content').addClass('large-content')
-}
-
-
-
-
-
-
-
-
-
-
-
 $(document).on('ready', function() {
 
 
