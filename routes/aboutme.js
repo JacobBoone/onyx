cacf7e76e75801fe28606281944b80628ec3ef3f
Jacob <jakeboone@aol.com>
2018-02-25 14:07:43 -0500

    text changes

--- routes/aboutme.js
+++ routes/aboutme.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('aboutme', { title: 'About Brittany' });
+  res.render('aboutme', { title: 'Brittany Love' });
 });
 
 
